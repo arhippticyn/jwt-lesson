5f4b710c51693a35ef5a9c6709168ce4974c3bed
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2026-01-10 12:22:44 +0200

    Add login and get user

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from models import *
 from db import Users, get_db
 import jwt
+from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
 
@@ -20,7 +21,7 @@ pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 def hash_password(password):
     return pwd_context.hash(password)
 
-def verify_password(hashed,password):
+def verify_password(hashed, password):
     return pwd_context.verify(password, hashed)
 
 @app.post('/register', response_model=UserResponse)
@@ -40,19 +41,47 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 
 @app.post('/token')
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user_db = db.query(Users).filter(Users.username == form_data.username).first()
+    user = db.query(Users).filter(Users.username == form_data.username).first()
 
-    if not user_db:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User not found')
+    if not user: 
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Username or password in not found')
     
-    if not verify_password(user_db.password, form_data.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='username or passsword not true')
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Username or password in not found')
+    
+    if not verify_password(user.password, form_data.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Username or password in not found')
     
     payload = {
-        'sub': user_db.username,
+        'sub': user.username,
         'exp': datetime.now(timezone.utc) + timedelta(ACCESS_TOKEN_EXPIRES)
     }
 
     access_token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITM)
 
     return {'access_token': access_token, 'type': 'bearer'}
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    creditials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},)
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITM])
+        username = payload.get('sub')
+
+        if username is None:
+            raise creditials_exception
+        
+    except InvalidTokenError:
+        raise creditials_exception
+    
+    user = db.query(Users).filter(Users.username == username).first()
+
+    if user is None: 
+        raise creditials_exception
+    
+    return user
+
+@app.get('/user', response_model=UserResponse)
+async def read_user(user: Users = Depends(get_current_user)):
+    return user
