9736ae806eaded94adf94905a2782034e9603ce0
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2026-01-13 23:55:16 +0200

    REmake in users table

--- db.py
+++ db.py
@@ -21,8 +21,7 @@ class Users(Base):
     username: Mapped[str] = mapped_column(String(50))
     email: Mapped[str] = mapped_column(String(200), unique=True)
     password: Mapped[str | None] = mapped_column()
-    google_id: Mapped[str] = mapped_column(nullable=True)
-    github_id: Mapped[str] = mapped_column(nullable=True)
+
 
 def get_db():
     db = SessionLocal()
